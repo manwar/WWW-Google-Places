4b7f841db3fc3a9a47b761eb136efd73a84d1a09
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2015-01-12 12:44:42 +0000

    - Added 'use 5.006;' line to the following test scripts:   - t/01-new.t   - t/02-search_place.t   - t/03-place_detail.t   - t/04-add_place.t   - t/meta-json.t   - t/meta-yml.t

--- lib/WWW/Google/Places.pm
+++ lib/WWW/Google/Places.pm
@@ -1,6 +1,6 @@
 package WWW::Google::Places;
 
-$WWW::Google::Places::VERSION   = '0.12';
+$WWW::Google::Places::VERSION   = '0.13';
 $WWW::Google::Places::AUTHORITY = 'cpan:MANWAR';
 
 =head1 NAME
@@ -9,7 +9,7 @@ WWW::Google::Places - Interface to Google Places API.
 
 =head1 VERSION
 
-Version 0.12
+Version 0.13
 
 =cut
 
